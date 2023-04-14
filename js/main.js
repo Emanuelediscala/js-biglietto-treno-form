375ad82c236157735a457b630bb75aead2058c2f
Emanuele Di Scala <manueldiscala@gmail.com>
2023-04-14 18:17:27 +0200

    feat: (variables,actions) Must add Annulla button

--- js/main.js
+++ js/main.js
@@ -5,27 +5,34 @@ function calcPrice() {
 let nomeCognome = document.getElementById("Nome&Cognome").value;
 document.getElementById("Nomepasseggero").innerText = nomeCognome;
 // INSERIMENTO ETA'
-let età = document.getElementById("età").value;
+let eta = document.getElementById("età").value;
 // INSERIMENTO KM
-let km = document.getElementById("Kmdapercorrere")
-    let generalprice = 0.21;
-    let pricexkm = generalprice * km;
-// NaN condition
-    if (isNaN(età) || isNaN(richiestaKm)) {
-        alert("Si accettano solo caratteri numerici")
+let km = document.getElementById("Kmdapercorrere").value;
+let generalprice = 0.21;
+let pricexkm = generalprice * km;
+let Numerodellacarrozza = Math.floor (Math.random() * 11 + 1 );
+let Offerta = "biglietto Standard"
+let CodicePrenotazione = Math.ceil (Math.random() * 10000 );
+// NaN/Null condition
+    if (isNaN(eta) || isNaN(km) || (eta=="") || (km=="") || (nomeCognome=="") ) {
+        alert("C'è un errore")
     }
     else  {
 // AGE CONDITION
-        if (età > 65) {
+        if (eta > 65) {
             pricexkm -= pricexkm / 100 * 40;
             alert("Hai ricevuto uno sconto del 40%")
         }
-        else if (età < 18) {
+        else if (eta < 18) {
             pricexkm -= pricexkm / 100 * 20;
             alert("Hai ricevuto uno sconto del 20%")
         }
-// REMOVE DECIMALS        
-        pricexkm = parseFloat(pricexkm).toFixed(2);
-        document.querySelector("#price").innerText = pricexkm + " €";
-    }
-    }
+// TICKETS DETAILS + DECIMAL REMOVALS
+    document.getElementById("numerodellacarrozza").append(Numerodellacarrozza);   
+    document.getElementById("tipodiofferta").innerText = Offerta;
+    document.getElementById("codiceprenotazione").innerText = CodicePrenotazione;
+    pricexkm = parseFloat(pricexkm).toFixed(2);
+    document.querySelector("#price").innerText = pricexkm + " €";
+}
+
+}
